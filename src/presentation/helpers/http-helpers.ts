8494b6d247b14381d0202304dcc3fb8b64604b0e
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-05 08:04:41 -0300

    refactor: refactor import http helpers

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,5 +1,5 @@
-import { type HttpResponse } from '../protocols/http'
-import { ServerError } from '../errors/server-error'
+import { type HttpResponse } from '../protocols'
+import { ServerError } from '../errors'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
