0c7aba2734ac2813f318bc40669d64f6c8b1cedb
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-05 09:37:55 -0300

    feat: ensure EmailValidatorAdapter returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,7 @@
+import { type EmailValidator } from '../presentation/protocols'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
