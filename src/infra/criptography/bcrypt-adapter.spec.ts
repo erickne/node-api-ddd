d44491f595b22c52e101569e20ed1fdf7249c74a
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-11 16:36:00 -0300

    test: ensure BcryptAdapter return a hash on success

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => { resolve('hash') })
+  }
+}))
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct value', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  test('Should return a has on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
+  })
+})
