fc5af1635b673651f29e4e7a44201e0d9855084a
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-12 07:38:45 -0300

    chore: add jest configs for mongo db

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
