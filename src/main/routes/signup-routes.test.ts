14ba84fccdaaa8ce74a73425a6ecd5dd2831378f
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-12 15:38:45 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    // @ts-expect-error error no typescript
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection.deleteMany({})
+  })
+
   test('Should return and account on success', async () => {
     await request(app)
       .post('/api/signup')
