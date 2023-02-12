2c5f27381b36231332628277b236a354d83a002c
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-12 16:17:52 -0300

    feat: ensure mongogb reconnect if client connection is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -5,6 +5,7 @@ export const MongoHelper = {
   uri: null as unknown as string,
 
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -14,9 +15,13 @@ export const MongoHelper = {
 
   async disconnect () {
     await this.client.close()
+    this.client = null
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   },
 
