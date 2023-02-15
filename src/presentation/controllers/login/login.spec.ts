e7c61a8ca7d364cceda217fb2287fbad3e912fc8
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-15 09:55:29 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,16 @@
+import { LoginController } from './login'
+import { badRequest } from '../../helpers/http-helpers'
+import { MissingParamError } from '../../errors'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'any_pw'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
