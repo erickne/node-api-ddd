2b0c481779d88656d61fa9694cb30ab34512bb58
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-15 09:31:37 -0300

    refactor: move sut creation to a helper method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -5,6 +5,9 @@ import { LogMongoRepository } from './log'
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
 
+  const makeSut = (): LogMongoRepository => {
+    return new LogMongoRepository()
+  }
   beforeAll(async () => {
     // @ts-expect-error error no typescript
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -19,7 +22,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
