47a4f9bd8da5d6a84a8eae725e5231b0488cac06
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-21 16:20:48 -0300

    test: ensure db-authentication throws if hasCompare throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -77,4 +77,10 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     await expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
+  test('Should throw if hashCompare throws', async () => {
+    const { sut, hashCompareStub } = makeSut()
+    jest.spyOn(hashCompareStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })
