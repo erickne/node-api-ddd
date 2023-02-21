000b7c0c921acd1652f43bb43b5a20336f999640
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-21 15:54:09 -0300

    feat: ensur db-authentication call load-account-by-email-repository with correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { type AccountModel } from '../../domain/models/account'
+
+export interface LoadAccountByEmailRepository {
+  load: (email: string) => Promise<AccountModel>
+}
---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import { type Authentication, type AuthenticationModel } from '../../../domain/usecases/authentication'
+import { type LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authentication: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authentication.email)
+    // @ts-expect-error
+    return null
+  }
+}
