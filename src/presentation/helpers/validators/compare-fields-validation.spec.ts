d6b643e362feeb5c8579ae0f175a75bcffaef7c4
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-21 07:47:18 -0300

    test: add tests to compare fields validation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+describe('Compare Fields Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'other_value' })
+    expect(error).toEqual((new InvalidParamError('fieldToCompare')))
+  })
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
