58af8f4def85ba1039c5629e67f27d1732be06df
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-23 20:51:05 -0300

    feat: ensure return an account on load by email succeeds

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,11 +2,18 @@ import { type AddAccountRepository } from '../../../../data/protocols/db/add-acc
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { type AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { type LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.findOne({ email })
+    return MongoHelper.map(result)
   }
 }
