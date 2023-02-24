bf1ce44ecfd86a2ebbbac57495492411c254fb3b
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-24 18:57:35 -0300

    chore: add docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:16
 WORKDIR /usr/src/server
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
-EXPOSE 500
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clea-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clea-node-api',
   port: process.env.POST ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'rTfks=%21'
 }
