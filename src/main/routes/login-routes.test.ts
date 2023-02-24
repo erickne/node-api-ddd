43f9cca7d50c9ed3dd92596bcbab8da8ad971383
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-24 14:06:50 -0300

    test: add login route returns 401 on invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'erick@teste.com',
+          password: 'erick123'
+        })
+        .expect(401)
+    })
   })
 })
