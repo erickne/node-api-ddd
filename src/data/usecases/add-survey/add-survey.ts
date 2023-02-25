fa30ec9ad369184c2feb825413795960a1023d8f
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-25 18:36:12 -0300

    test: ensure add-survey call add-survey-repository with correct values

---
+++ src/data/usecases/add-survey/add-survey.ts
@@ -0,0 +1,12 @@
+import { type AddSurvey, type AddSurveyModel } from '../../../domain/usecases/add-survey'
+import { type AddSurveyRepository } from '../../protocols/db/survey/add-survey-repository'
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (
+    private readonly addSurveyRepository: AddSurveyRepository
+  ) {}
+
+  async add (data: AddSurveyModel): Promise<void> {
+    await this.addSurveyRepository.add(data)
+  }
+}
