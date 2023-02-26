81e17fc132e80afc32e8758c94909fffa5c6009a
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-26 18:04:10 -0300

    feat: ensure survey-mongo-repository return a survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,11 @@
+import { type AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { type AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (surveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const result = await surveyCollection.insertOne(surveyData)
+    return MongoHelper.map(result.ops[0])
+  }
+}
