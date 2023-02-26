6742986e31733ab3a89fdf4912164b89d92fc31d
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-02-26 18:50:02 -0300

    feat: ensure auth-middleware calls LoadAccountByToken with correct token

---
+++ src/domain/usecases/load-account-by-token.ts
@@ -0,0 +1,5 @@
+import { type AccountModel } from '../models/account'
+
+export interface LoadAccountByToken {
+  load: (accessToken: string, role?: string) => Promise<AccountModel>
+}
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -2,10 +2,18 @@ import { type Middleware } from '../protocols/middleware'
 import { type HttpRequest, type HttpResponse } from '../protocols'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helpers'
+import { type LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 
 export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.['x-access-token']
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken)
+    }
     return forbidden(new AccessDeniedError())
-    // return await new Promise((resolve, reject) => { resolve(null) })
   }
 }
