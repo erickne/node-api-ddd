3ef87b1236698b0a91be4c0876196b02815c808e
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-03-03 19:52:34 -0300

    test: ensure LoadAccountByToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -56,4 +56,12 @@ describe('Auth Middleware', () => {
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
+  test('Should return 403 if LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load')
+      // @ts-expect-error
+      .mockReturnValueOnce(new Promise((resolve, reject) => { resolve(null) }))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
 })
