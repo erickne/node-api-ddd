66697ce0955a6672255eca360d515c67a95c054b
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-03-04 10:14:04 -0300

    feat: ensure load-surveys-controller return 204 if there is no content

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,6 +1,6 @@
 import { type Controller, type HttpRequest, type HttpResponse } from '../../../protocols'
 import { type LoadSurveys } from '../../../../domain/usecases/load-surveys'
-import { ok, serverError } from '../../../helpers/http/http-helpers'
+import { noContent, ok, serverError } from '../../../helpers/http/http-helpers'
 
 export class LoadSurveysController implements Controller {
   constructor (
@@ -10,7 +10,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
     }
