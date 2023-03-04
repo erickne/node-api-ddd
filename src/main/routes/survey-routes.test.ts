f63d2cff31ceb9154c8a7b1b77326a96eeeb09d6
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-03-03 22:59:46 -0300

    test: ensure Survey Routes returns 204 on admin add survey

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection = Collection
+let accountCollection = Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -16,6 +19,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /signup', () => {
@@ -32,6 +37,34 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+    test('Should return 200 on admin add survey ', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'erick',
+        email: 'erick@teste.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'https://image.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
     })
   })
 })
