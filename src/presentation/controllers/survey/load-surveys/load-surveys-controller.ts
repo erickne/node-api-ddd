39a966b7706c956d673964006f48a13fdeedf1f3
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2023-03-04 09:04:30 -0300

    test: ensue load-surveys-controller return 500 on error

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,6 +1,6 @@
 import { type Controller, type HttpRequest, type HttpResponse } from '../../../protocols'
 import { type LoadSurveys } from '../../../../domain/usecases/load-surveys'
-import { ok } from '../../../helpers/http/http-helpers'
+import { ok, serverError } from '../../../helpers/http/http-helpers'
 
 export class LoadSurveysController implements Controller {
   constructor (
@@ -8,7 +8,11 @@ export class LoadSurveysController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
